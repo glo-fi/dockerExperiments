2d52d7875d7853a24cb58557f8cd970cb534ac6e
Rob Flood <rflood@tcd.ie>
2022-01-19 12:18:33 +0000

    Following the online example more closely

--- profile.py
+++ profile.py
@@ -2,6 +2,8 @@
 Basic Geni-lib example
 
 Goal: Get Apache running via this scripting interface.
+
+NB: Tried to get Docker container running on raw pc and connected to another raw pc. That isn't working and it actually seems easier to connect it to another docker container running on that pc.
 """
 
 
@@ -18,8 +20,11 @@ request = portal.context.makeRequestRSpec()
 # Create LAN to put containers into
 lan = request.LAN("lan")
 
-host = request.RawPC("host")
-host.hardware_type = "d430"
+node1 = request.DockerContainer("node0") # Node running Apache in Container
+
+host0 = request.RawPC("host0")
+host0.hardware_type = "d430"
+
 
 # Add a DockerContainer to the request
 node0 = request.DockerContainer("node0") # Node running Apache in Container
@@ -30,23 +35,24 @@ node0.exclusive = True
 
 
 # Add regular node for testing apache
-node1 = request.RawPC("node1")
+host1 = request.RawPC("host1")
 
 
-iface1 = host.addInterface("if1")
-iface1.component_id = "eth1"
-iface1.addAddress(rspec.IPv4Address("192.168.1.1", "255.255.255.0"))
+iface1 = node0.addInterface("if1")
+#iface1.component_id = "eth1"
+#iface1.addAddress(rspec.IPv4Address("192.168.1.1", "255.255.255.0"))
 
 iface2 = node1.addInterface("if1")
-iface2.component_id = "eth1"
-iface2.addAddress(rspec.IPv4Address("192.168.1.2", "255.255.255.0"))
+#iface2.component_id = "eth1"
+#iface2.addAddress(rspec.IPv4Address("192.168.1.2", "255.255.255.0"))
 
 lan.addInterface(iface1)
 
 lan.addInterface(iface2)
 
 # Add Docker Contaner to host
-node0.InstantiateOn(host.client_id)
+node0.InstantiateOn(host0.client_id)
+node1.InstantiateOn(host1.client_id)
 
 # Write the request in rspec format
 node1.addService(rspec.Execute(shell="bash", command='sudo docker run -dit --name apache-app -p 8080:80 -v /local/repository/htdocs:/usr/local/apache2/htdocs/ httpd:2.4'))
