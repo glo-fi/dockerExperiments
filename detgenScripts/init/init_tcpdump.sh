2cfb7eca5e28e62129afaba6097797e90d417cd0
Rob Flood <rflood@tcd.ie>
2022-01-20 13:45:16 +0000

    Please work this time

--- detgenScripts/init/init_tcpdump.sh
+++ detgenScripts/init/init_tcpdump.sh
@@ -4,4 +4,4 @@ TIME=$(date +'%T' | sed 's/:/_/g')
 CONTAINER=$1
 ID=$2
 
-sudo docker run -v /local/repository/collectedData:/data --network=container:${CONTAINER} --name docker_tcpdump${ID} docker-tcpdump ' -v -w /data/${TIME}_${CONTAINER}.pcap'
+sudo docker run -v /local/repository/collectedData:/data --network=container:${CONTAINER} --name docker_tcpdump${ID} docker-tcpdump ' -i eth0 -w /data/${TIME}_${CONTAINER}.pcap'
