13ac746deebde9e1026403cdaf0f89ad37de9d5f
Rob Flood <rflood@tcd.ie>
2022-01-21 10:31:54 +0000

    Think the tcpdump containers will attach properly now

--- detgenScripts/init/init_tcpdump.sh
+++ detgenScripts/init/init_tcpdump.sh
@@ -4,4 +4,11 @@ TIME=$(date +'%T' | sed 's/:/_/g')
 CONTAINER=$1
 ID=$2
 
-sudo docker run -v /local/repository/collectedData:/data --network=container:${CONTAINER} --name docker_tcpdump${ID} docker-tcpdump ' -i eth0 -w /data/${TIME}_${CONTAINER}.pcap'
+VAR1=" -i eth0 -w /data/${TIME}_${CONTAINER}.pcap"
+echo $VAR1
+VAR2="sudo docker run -v /local/repository/collectedData:/data --network=container:${CONTAINER} --name docker-tcpdump${ID} docker-tcpdump"
+echo $VAR2
+
+`$VAR2 $VAR1`
+
+# sudo docker run -v /local/repository/collectedData:/data --network=container:${CONTAINER} --name docker_tcpdump${ID} docker-tcpdump ' -i eth0 -w /data/${TIME}_${CONTAINER}.pcap'
